6302f7f5cf94fb8534fae4035d3a453a7d59ed4d
Pekka Heikkinen <pekka.tilit@aijai.org>
2019-02-24 12:08:15 +0200

    Check for hash conflicts

--- Editor/PersistentTriggerStateEditor.cs
+++ Editor/PersistentTriggerStateEditor.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine;
 using UnityEditor;
+using UnityEditor.SceneManagement;
+using System.IO;
 
 namespace Aijai.Persistence
 {
     [CustomEditor(typeof(PersistentTriggerState))]
     public class PersistentTriggerStateEditor : Editor
     {
+        List<string> sceneConflicts;
+
+        private void OnEnable()
+        {
+            sceneConflicts = new List<string>();
+            var hashes = new HashSet<int>();
+            foreach(var asset in AssetDatabase.FindAssets("t:scene"))
+            {
+                var p = AssetDatabase.GUIDToAssetPath(asset);
+                var hash = new PropertyName(Path.GetFileNameWithoutExtension(p)).GetHashCode();
+                if (!hashes.Add(hash))
+                {
+                    sceneConflicts.Add(p);
+                }
+            }
+        }
+
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
+
+            foreach (var conflict in sceneConflicts)
+            {
+                EditorGUILayout.HelpBox(string.Format("Hash conflict detected with {0}.\nRename scene to fix.", conflict), MessageType.Error);
+            }
 
             if (Application.isPlaying)
             {
